d4789239d21c45e55d36dcb9e6f4f839a7005452
realstefcortinovis <real.stefcor@yahoo.com>
2025-11-18 10:46:37 +0000

    Deploying to gh-pages from @ realstefcortinovis/realstefcortinovis.github.io@6b9790490ad2c8ebe52353eafac9c04763accb74 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,13 +412,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-our-paper-symdiff-equivariant-diffusion-via-stochastic-symmetrisation-was-accepted-at-iclr-2025",
+          title: 'Our paper “SymDiff: Equivariant Diffusion via Stochastic Symmetrisation” was accepted at ICLR 2025....',
           description: "",
           section: "News",},{id: "news-our-paper-symdiff-equivariant-diffusion-via-stochastic-symmetrisation-was-accepted-at-iclr-2025",
           title: 'Our paper “SymDiff: Equivariant Diffusion via Stochastic Symmetrisation” was accepted at ICLR 2025....',
